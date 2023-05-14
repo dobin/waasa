9927e1cc5a4148610e341e9756cda3f091b638d6
dobinrutis <dobin@broken.ch>
2023-05-14 23:03:23 +0200

    tests: add test project

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var app = new waasa.App();
+
+        }
+    }
+}
